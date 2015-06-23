ec8217d25c260837c388de6f511559beeda9f908
piyu2115 <priyushet@gmail.com>
2015-06-23 15:46:24 +0530

    this is commit two

--- warmuplab/src/warmuplab/Demo_main.java
+++ warmuplab/src/warmuplab/Demo_main.java
@@ -5,7 +5,8 @@ public class Demo_main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		Hello h=new Hello();
+		h.display();
 	}
 
 }
---
+++ warmuplab/src/warmuplab/Hello.java
@@ -0,0 +1,8 @@
+package warmuplab;
+
+public class Hello {
+
+	public void display(){
+		System.out.println("yeah!!!");
+	}
+}
